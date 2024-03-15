92f8e9f9ffc35055e2a9b1cecd3fc8f7bdc7922b
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2024-03-14 21:38:32 -0400

    Banco de Dados H2 Database e Seed

--- src/main/java/com/anpede/entities/Associado.java
+++ src/main/java/com/anpede/entities/Associado.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_ASSOCIADO (nome, cpf, data_nascimento, telefone, email, endereco) VALUES ('Maria Aparecida', '123.456.789-78', '1990-10-21', '(67) 98888-5555', 'maria@gmail.com', 'Av. Dourados, 123, Centro, Naviraí-MS');
+INSERT INTO TB_ASSOCIADO (nome, cpf, data_nascimento, telefone, email, endereco) VALUES ('João Aclecio da Silva', '123.456.789-78', '1990-10-21', '(67) 98888-5555', 'joao@gmail.com', 'Av. Dourados, 123, Centro, Naviraí-MS');
+INSERT INTO TB_ASSOCIADO (nome, cpf, data_nascimento, telefone, email, endereco) VALUES ('Pedro Henrique', '123.456.789-78', '1990-10-21', '(67) 98888-5555', 'pedro@gmail.com', 'Av. Dourados, 123, Centro, Naviraí-MS');
+INSERT INTO TB_ASSOCIADO (nome, cpf, data_nascimento, telefone, email, endereco) VALUES ('Catarina Alcantara', '123.456.789-78', '1990-10-21', '(67) 98888-5555', 'catarina@gmail.com', 'Av. Dourados, 123, Centro, Naviraí-MS');
+INSERT INTO TB_ASSOCIADO (nome, cpf, data_nascimento, telefone, email, endereco) VALUES ('Izabel de Lima', '123.456.789-78', '1990-10-21', '(67) 98888-5555', 'izabel@gmail.com', 'Av. Dourados, 123, Centro, Naviraí-MS');
+
