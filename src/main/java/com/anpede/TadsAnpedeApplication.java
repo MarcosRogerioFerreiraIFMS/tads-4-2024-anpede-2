c6282935babb68bcb28ce43b5e7c769aa32556f2
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2024-07-25 21:46:01 -0400

    Cors e Swagger

--- src/main/java/com/anpede/TadsAnpedeApplication.java
+++ src/main/java/com/anpede/TadsAnpedeApplication.java
@@ -2,10 +2,20 @@ package com.anpede;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TadsAnpedeApplication {
+@EnableWebMvc
+public class TadsAnpedeApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TadsAnpedeApplication.class, args);
 	}
